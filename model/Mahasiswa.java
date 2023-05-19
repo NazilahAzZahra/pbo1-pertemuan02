e49120fb16e6cb9494c72b0c66ef69987992b606
NazilahAzZahra <nazilah.zahraa@gmail.com>
2023-05-19 21:41:59 +0800

    Memanggil method dari banyak Class di package lain

---
+++ model/Mahasiswa.java
@@ -0,0 +1,11 @@
+package model;
+
+public class Mahasiswa{
+    public static void tampilanInfo(){
+        System.out.println();
+        System.out.println("== Data Mahasiswa =================");
+        System.out.println("NPM  : 2110010477");
+        System.out.println("Nama : Nazilah Az Zahra");
+        System.out.println("===============================");
+    }
+}
